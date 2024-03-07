a8a01ab14540f44785c318e42ab81c454eb54d57
taufik69 <taufik35-2915@diu.edu.bd>
2024-03-08 02:21:56 +0600

    promise chaining

--- main.js
+++ main.js
@@ -41,73 +41,44 @@
 //   });
 // }
 
-function one(callback, value) {
-  setTimeout(() => {
-    console.log("this is function one");
-    callback(value + 10);
-  }, 2000);
-}
-
-function two(value, callback) {
-  setTimeout(() => {
-    console.log("this is function two");
-    callback(value + 10);
-  }, 100);
-}
-
-function three(value, callback) {
-  setTimeout(() => {
-    console.log("this is function three");
-    callback(value + 10);
-  }, 200);
-}
-
-function four(value, callback) {
-  setTimeout(() => {
-    console.log("this is function four");
-    callback(value + 10);
-  }, 200);
-}
-
-function five(value, callback) {
-  setTimeout(() => {
-    console.log("this is function five");
-    callback(value + 10);
-  }, 300);
-}
-
-function six(value, callback) {
-  setTimeout(() => {
-    console.log("this is function six");
-    callback(value + 10);
-  }, 3000);
-}
-
-function seven(value, callback) {
-  setTimeout(() => {
-    console.log("this is function seven");
-    callback(value + 10);
-  }, 1000);
-}
-
-function eight(value) {
-  setTimeout(() => {
-    console.log("Our final value is : ", value);
-  }, 200);
-}
-
-one((result) => {
-  two(result, (result) => {
-    three(result, (result) => {
-      four(result, (result) => {
-        five(result, (result) => {
-          six(result, (result) => {
-            seven(result, (result) => {
-              eight(result);
-            });
-          });
-        });
-      });
-    });
+function fun1() {
+  return new Promise((reslove, reject) => {
+    setTimeout(() => {
+      console.log("this is sucessfully fetching data 1");
+      reslove("one");
+    }, 2000);
   });
-}, 10);
+}
+
+function fun2() {
+  return new Promise((reslove, reject) => {
+    setTimeout(() => {
+      console.log("this is sucessfully fetching data 2");
+      reslove();
+    }, 1000);
+  });
+}
+
+function fun3() {
+  return new Promise((reslove, reject) => {
+    setTimeout(() => {
+      console.log("this is sucessfully fetching data 3");
+    }, 200);
+  });
+}
+
+fun1()
+  .then((resultFromFun1) => {
+    console.log("Result from fun1:", resultFromFun1);
+    return fun2(); // Return a promise from fun2()
+  })
+  .then(() => {
+    console.log("Fun2 completed.");
+    return fun3(); // Return a promise from fun3()
+  })
+  .then(() => {
+    console.log("All functions completed successfully.");
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
