19278c02c17ce707cf1d9859022af893e1dfeac0
taufik69 <taufik35-2915@diu.edu.bd>
2024-03-08 03:12:05 +0600

    promise done

--- main.js
+++ main.js
@@ -41,44 +41,85 @@
 //   });
 // }
 
-function fun1() {
+// function fun1() {
+//   return new Promise((reslove, reject) => {
+//     setTimeout(() => {
+//       reslove("this is sucessfully fetching data 1");
+//     }, 2000);
+//   });
+// }
+
+// function fun2() {
+//   return new Promise((reslove, reject) => {
+//     setTimeout(() => {
+//       reslove("this is sucessfully fetching data 2");
+//     }, 1000);
+//   });
+// }
+
+// function fun3() {
+//   return new Promise((reslove, reject) => {
+//     setTimeout(() => {
+//       reslove("this is sucessfully fetching data 3");
+//     }, 200);
+//   });
+// }
+
+// fun1()
+//   .then((resultFromFun1) => {
+//     console.log(resultFromFun1);
+//     return fun2(); // Return a promise from fun2()
+//   })
+//   .then((resultFromFun2) => {
+//     console.log(resultFromFun2);
+//     return fun3(); // Return a promise from fun3()
+//   })
+//   .then((resultFromFun3) => {
+//     console.log(resultFromFun3);
+//   })
+//   .catch((error) => {
+//     console.error("An error occurred:", error);
+//   });
+
+// now make a another promise
+
+const cart = ["shoes ", "pants", "shirt"];
+createOrder(cart)
+  .then((getresult) => {
+    return getresult;
+  })
+  .then((getresultfrom1) => {
+    return procedPayment(getresultfrom1);
+  })
+  .then((paymentInfo) => {
+    console.log("from chain", paymentInfo);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("sob finished");
+  });
+
+// make a promise object // or create a promise
+function createOrder(cart) {
   return new Promise((reslove, reject) => {
+    let status = false;
+    if (status) {
+      reslove(cart);
+    } else {
+      const err = new Error("Cart is not valid");
+      reject(err);
+    }
+  });
+}
+
+// make another promise funciton
+
+function procedPayment(getinfofrom1stfunction) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      reslove("this is sucessfully fetching data 1");
+      resolve("payment done " + getinfofrom1stfunction);
     }, 2000);
   });
 }
-
-function fun2() {
-  return new Promise((reslove, reject) => {
-    setTimeout(() => {
-      reslove("this is sucessfully fetching data 2");
-    }, 1000);
-  });
-}
-
-function fun3() {
-  return new Promise((reslove, reject) => {
-    setTimeout(() => {
-      reslove("this is sucessfully fetching data 3");
-    }, 200);
-  });
-}
-
-fun1()
-  .then((resultFromFun1) => {
-    console.log(resultFromFun1);
-    return fun2(); // Return a promise from fun2()
-  })
-  .then((resultFromFun2) => {
-    console.log(resultFromFun2);
-    return fun3(); // Return a promise from fun3()
-  })
-  .then((resultFromFun3) => {
-    console.log(resultFromFun3);
-  })
-  .catch((error) => {
-    console.error("An error occurred:", error);
-  });
-
-// now make a another promise
