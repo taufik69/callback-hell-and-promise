fe80606cf38add8d1985898e60d39b19bc3e0287
taufik69 <taufik35-2915@diu.edu.bd>
2024-03-08 02:30:35 +0600

    promise chaining updated

--- main.js
+++ main.js
@@ -44,8 +44,7 @@
 function fun1() {
   return new Promise((reslove, reject) => {
     setTimeout(() => {
-      console.log("this is sucessfully fetching data 1");
-      reslove("one");
+      reslove("this is sucessfully fetching data 1");
     }, 2000);
   });
 }
@@ -53,8 +52,7 @@ function fun1() {
 function fun2() {
   return new Promise((reslove, reject) => {
     setTimeout(() => {
-      console.log("this is sucessfully fetching data 2");
-      reslove();
+      reslove("this is sucessfully fetching data 2");
     }, 1000);
   });
 }
@@ -62,23 +60,25 @@ function fun2() {
 function fun3() {
   return new Promise((reslove, reject) => {
     setTimeout(() => {
-      console.log("this is sucessfully fetching data 3");
+      reslove("this is sucessfully fetching data 3");
     }, 200);
   });
 }
 
 fun1()
   .then((resultFromFun1) => {
-    console.log("Result from fun1:", resultFromFun1);
+    console.log(resultFromFun1);
     return fun2(); // Return a promise from fun2()
   })
-  .then(() => {
-    console.log("Fun2 completed.");
+  .then((resultFromFun2) => {
+    console.log(resultFromFun2);
     return fun3(); // Return a promise from fun3()
   })
-  .then(() => {
-    console.log("All functions completed successfully.");
+  .then((resultFromFun3) => {
+    console.log(resultFromFun3);
   })
   .catch((error) => {
     console.error("An error occurred:", error);
   });
+
+// now make a another promise
